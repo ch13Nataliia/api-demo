0590c2a2f5be64823ca715e65cf1ec6ade6bf365
Nataliia Tsirul <114382984+ch13Nataliia@users.noreply.github.com>
2023-04-10 20:03:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ require('dotenv').config();
 const server = require('./server');
 
 
-const { PORT = 3001,
-NODE_ENV='development' } = process.env;
+const { PORT = 3001} = process.env;
 
 server.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
