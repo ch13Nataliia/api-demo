3fa761c1fd7831699606df07d8812ca1a425f55b
Nataliia Tsirul <114382984+ch13Nataliia@users.noreply.github.com>
2023-04-10 20:02:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const server = require('./server');
 
 
-const { PORT = 3333,
+const { PORT = 3001,
 NODE_ENV='development' } = process.env;
 
 server.listen(PORT, () => {
